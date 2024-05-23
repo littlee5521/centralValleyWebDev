a4b61bdc9800035754c5571ae8297b4d218fdb82
Martin Rodriguez <68254461+littlee5521@users.noreply.github.com>
2024-05-22 22:37:25 -0700

    Submit button changed, inputs now clear on send

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -4,6 +4,9 @@ import emailsJs from "@emailjs/browser";
 
 export default function Page() {
   const formRef = useRef<HTMLFormElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
   useEffect(() => {
     emailsJs.init({
       publicKey: "Z9Aj9dXcdHGYivOWN",
@@ -19,6 +22,9 @@ export default function Page() {
       })
       .then(() => {
         console.log("Sucess");
+        if (nameRef.current) nameRef.current.value = "";
+        if (emailRef.current) emailRef.current.value = "";
+        if (messageRef.current) messageRef.current.value = "";
       });
   };
   return (
@@ -33,17 +39,23 @@ export default function Page() {
           className="mb-5 pl-5 rounded-md h-10 text-black"
           type="text"
           name="user_name"
+          ref={nameRef}
         />
         <label className=" text-2xl mb-2">Email</label>
         <input
           className="mb-5 pl-5 rounded-md h-10 text-black"
           type="email"
           name="user_email"
+          ref={emailRef}
         />
         <label className=" text-2xl mb-2">Message</label>
-        <textarea className=" rounded-md pl-5 h-40 text-black" name="message" />
+        <textarea
+          ref={messageRef}
+          className=" rounded-md pl-5 h-40 text-black"
+          name="message"
+        />
         <input
-          className=" mt-10 text-xl  h-10 text-black  bg-gray-400"
+          className=" mt-10 text-xl mb-10  h-10 text-white border border-white bg-black"
           type="submit"
           content="Hello"
           value="Send"
